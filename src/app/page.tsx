3b2172f26f4b6d44ae521952c6a333fa1b69d917
QuentinSpace <pro.quentint@gmail.com>
2024-03-22 03:31:11 +0100

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<main className="h-screen w-screen flex items-center justify-center">
+		<main className="h-screen w-screen flex items-center justify-center flex-col">
 			<div className="font-semibold">Mooya</div>
 			<div>Site en construction</div>
 		</main>
