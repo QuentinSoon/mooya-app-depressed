e32f99d26fa1e3094b21f89cded89428d19daea6
QuentinSpace <pro.quentint@gmail.com>
2024-03-22 19:26:29 +0100

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
+
+import LOGO from '../../public/arrière-plan.png';
+
 export default function Home() {
 	return (
-		<main className="h-screen w-screen flex items-center justify-center flex-col">
-			<div className="font-semibold">Mooya</div>
-			<div>Site en construction</div>
-			<div>Update: 1.0.1</div>
+		<main className="h-screen w-screen flex flex-col">
+			<div className="h-16 border flex flex-row items-center gap-4 px-8">
+				<div>
+					<Image src={LOGO} height={32} alt="logo" />
+				</div>
+				<div className="font-semibold">Mooya</div>
+			</div>
+			<div className="grow flex items-center justify-center flex-col">
+				<div className="font-semibold">Mooya</div>
+				<div>Site en construction</div>
+				<div>Update: 1.0.1</div>
+			</div>
 		</main>
 	);
 }
