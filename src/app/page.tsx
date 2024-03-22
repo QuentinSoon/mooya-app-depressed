727c52141fe9b7871efba93883f3e2fa043b6c42
QuentinSpace <pro.quentint@gmail.com>
2024-03-22 18:23:49 +0100

    storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
 		<main className="h-screen w-screen flex items-center justify-center flex-col">
 			<div className="font-semibold">Mooya</div>
 			<div>Site en construction</div>
+			<div>Update: 1.0.1</div>
 		</main>
 	);
 }
